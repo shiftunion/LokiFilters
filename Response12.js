e1a61d1c35ddfdf5ac58b49b2ff4d45850d68410
ShiftUnion <nick.cahill@gmail.com>
2018-02-18 22:15:32 +1300

    Got this sh!t smoking slow... bit working

---
+++ Response12.js
@@ -0,0 +1,123 @@
+
+var responses = [
+    {
+        "responseId": 0,
+        "animalColor": "green",
+        "name": "Alison",
+        "gender": "female",
+        "company": "Biolive",
+        "state": "Marshall Islands",
+        "responseDate": "2018-08-07",
+        "companyName": "Inear"
+    },
+    {
+        "responseId": 1,
+        "animalColor": "cyan",
+        "name": "Katrina",
+        "gender": "female",
+        "company": "Suremax",
+        "state": "Illinois",
+        "responseDate": "2018-11-05",
+        "companyName": "Comstruct"
+    },
+    {
+        "responseId": 2,
+        "animalColor": "green",
+        "name": "Minnie",
+        "gender": "female",
+        "company": "Intergeek",
+        "state": "Rhode Island",
+        "responseDate": "2015-12-24",
+        "companyName": "Vixo"
+    },
+    {
+        "responseId": 3,
+        "animalColor": "blue",
+        "name": "Hazel",
+        "gender": "female",
+        "company": "Automon",
+        "state": "West Virginia",
+        "responseDate": "2018-03-13",
+        "companyName": "Snacktion"
+    },
+    {
+        "responseId": 4,
+        "animalColor": "green",
+        "name": "Edna",
+        "gender": "female",
+        "company": "Isonus",
+        "state": "Pennsylvania",
+        "responseDate": "2016-03-09",
+        "companyName": "Exposa"
+    },
+    {
+        "responseId": 5,
+        "animalColor": "yellow",
+        "name": "Jessica",
+        "gender": "female",
+        "company": "Zillanet",
+        "state": "South Dakota",
+        "responseDate": "2018-07-18",
+        "companyName": "Tersanki"
+    },
+    {
+        "responseId": 6,
+        "animalColor": "red",
+        "name": "Sellers",
+        "gender": "male",
+        "company": "Puria",
+        "state": "Montana",
+        "responseDate": "2018-07-13",
+        "companyName": "Amril"
+    },
+    {
+        "responseId": 7,
+        "animalColor": "yellow",
+        "name": "Carissa",
+        "gender": "female",
+        "company": "Snacktion",
+        "state": "Arkansas",
+        "responseDate": "2015-08-19",
+        "companyName": "Maineland"
+    },
+    {
+        "responseId": 8,
+        "animalColor": "yellow",
+        "name": "Mathews",
+        "gender": "male",
+        "company": "Comvoy",
+        "state": "Massachusetts",
+        "responseDate": "2018-10-17",
+        "companyName": "Talkalot"
+    },
+    {
+        "responseId": 9,
+        "animalColor": "brown",
+        "name": "Bertie",
+        "gender": "female",
+        "company": "Codact",
+        "state": "Minnesota",
+        "responseDate": "2017-08-22",
+        "companyName": "Vixo"
+    },
+    {
+        "responseId": 10,
+        "animalColor": "blue",
+        "name": "Graves",
+        "gender": "male",
+        "company": "Bedder",
+        "state": "Indiana",
+        "responseDate": "2017-06-23",
+        "companyName": "Inear"
+    },
+    {
+        "responseId": 11,
+        "animalColor": "cyan",
+        "name": "Stella",
+        "gender": "female",
+        "company": "Pearlessa",
+        "state": "Louisiana",
+        "responseDate": "2015-11-17",
+        "companyName": "Voratak"
+    }
+];
